f777059d38f7a2d6750bbd216defdbb8cc8b8b4a
Diana Jordan <dianacjordan@gmail.com>
2023-05-16 22:20:03 -0400

    basic client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import axios from "axios"
+import { useAuth0 } from '@auth0/auth0-react'
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="text-3xl font-bold underline">
+        replace with navbar
+      </h1>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>    </div>
   );
 }
 
