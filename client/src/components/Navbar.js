fa7091891ae46059d1bf464739b21fbe47141af6
Diana Jordan <dianacjordan@gmail.com>
2023-05-21 12:02:27 -0400

    logo and icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import logo from '../images/vicariously_logo.png'
 import Login from './Login';
 import Logout from './Logout';
 import Signup from './Signup';
@@ -12,9 +12,10 @@ const Navbar = ({ userData }) => {
 
   return (
     <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full ">
-      <div className="mb-2 sm:mb-0">
-        <NavLink className="text-2xl no-underline text-grey-darkest hover:text-blue-dark" to='/'>Vicariously</NavLink>
-      </div>
+     <NavLink to='/' className="flex items-center">
+                    <img src={logo} width={150} height={100} alt='Vicariously logo' />
+                </NavLink>
+
       <div>
         <NavLink className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-5" to='/'>Home</NavLink>
       </div>
