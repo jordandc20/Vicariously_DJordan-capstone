e255a5ac67d474852d91156b56fd9f4899972e4b
Diana Jordan <dianacjordan@gmail.com>
2023-05-22 21:18:53 -0400

    cleanup header

--- client/src/components/Delete.js
+++ client/src/components/Delete.js
@@ -23,7 +23,7 @@ const Delete = ({ onDelete, onFormClose, idToDel, path, name }) => {
     return (
         <div className="grid place-items-center  bg-yellow-50 ">
             <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 ">
-                <h1>Do you want to delete Note:  {name}</h1>
+                <h1>Do you want to delete:  {name}</h1>
                 <button type="submit" className="w-full text-white-100 bg-emerald-400 hover:bg-emerald-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center " onClick={(e) => { e.stopPropagation(); handleYes() }}>Yes</button>
                 <button type="reset" className="w-full text-white-100 bg-emerald-400 hover:bg-emerald-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center " value="Cancel" onClick={(e) => {
                     e.stopPropagation();
