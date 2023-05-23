5340e4ffb8d5bc777f02ea673a49d648c4cfb064
Diana Jordan <dianacjordan@gmail.com>
2023-05-23 12:38:30 -0400

    fix url period bug in seed, date bug in editform

--- client/src/components/CityCard.js
+++ client/src/components/CityCard.js
@@ -16,38 +16,40 @@ const CityCard = ({ cityData, onDelCity, handleEditCity }) => {
   const navigate = useNavigate()
   const { city_name, city_notes, country, id, locations, user_id } = cityData;
 
+  if (!cityData) { return <div>Loading city data...</div> }
+
+  // render loading message
+  if (isLoading) { return <div>Loading ...</div> }
+
 
   function handleCityCardClick(e) {
     navigate(`/users/${params.userId}/cities/${id}`)
   }
-  
- // render loading message
-  if (isLoading) { return <div>Loading ...</div> }
+
 
   return (
     < div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-50"   >
-    <div onClick={handleCityCardClick}>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 capitalize">{country}</div>
-        <p className="text-gray-700 text-base capitalize">{city_name}</p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">locations: ##</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">category1</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">category2</span>
-      </div></div>
+      <div onClick={handleCityCardClick}>
+        <div className="px-6 py-4">
+          <h3 className="text-gray-700 text-xl text-base font-bold capitalize ">{city_name}</h3>
+          <p className=" text-m mb-2 capitalize">{country}</p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">locations: {locations.length}</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">city notes: {city_notes.length}</span>
+        </div></div>
       {(isAuthenticated && Number(params.userId) === userData.id) && (<div>
-        <div>< button className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-50" onClick={(e) => {e.stopPropagation(); setExpandEditCity(true)}}>Edit City</button>
-        <ReactModal  appElement={document.getElementById('root') || undefined}  isOpen={expandEditCity} contentLabel="Edit City Modal" onRequestClose={() => setExpandEditCity(false)}>
-          <CityForm  locationData={cityData} type='editCity' onFormClose={() => setExpandEditCity(false)} onSubmit={handleEditCity} />
-        </ReactModal>
-      </div>
-      <div>
-        < button className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-50" onClick={(e) => {e.stopPropagation(); setIsOpen(true)}}>Delete City</button>
-        <ReactModal appElement={document.getElementById('root') || undefined}  isOpen={isOpen} contentLabel="Delete City Modal" onRequestClose={() => setIsOpen(false)}>
-          <Delete idToDel={id} path ='cities' name={city_name} onFormClose={() => setIsOpen(false)} onDelete={  onDelCity } />
-        </ReactModal>
-      </div>
+        <div>< button className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-50" onClick={(e) => { e.stopPropagation(); setExpandEditCity(true) }}>Edit City</button>
+          <ReactModal appElement={document.getElementById('root') || undefined} isOpen={expandEditCity} contentLabel="Edit City Modal" onRequestClose={() => setExpandEditCity(false)}>
+            <CityForm locationData={cityData} type='editCity' onFormClose={() => setExpandEditCity(false)} onSubmit={handleEditCity} />
+          </ReactModal>
+        </div>
+        <div>
+          < button className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-50" onClick={(e) => { e.stopPropagation(); setIsOpen(true) }}>Delete City</button>
+          <ReactModal appElement={document.getElementById('root') || undefined} isOpen={isOpen} contentLabel="Delete City Modal" onRequestClose={() => setIsOpen(false)}>
+            <Delete idToDel={id} path='cities' name={city_name} onFormClose={() => setIsOpen(false)} onDelete={onDelCity} />
+          </ReactModal>
+        </div>
       </div>)}
     </div >
   )
