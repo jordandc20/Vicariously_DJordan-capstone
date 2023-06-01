1926351838b300e1ebb8ea4f20e27a00a4b6a653
Diana Jordan <dianacjordan@gmail.com>
2023-06-01 19:36:23 -0400

    home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,25 +1,29 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import beach from '../images/IMG_5877.jpg'
+import temple from '../images/IMG_5728.jpg'
+import seoul from '../images/IMG_7654.jpg'
+import cities from '../images/cities.png'
+import cityNotes from '../images/city-notes.png'
 import { ArrowDownIcon } from '@heroicons/react/24/outline'
 
 const Home = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-   
-    let count = 0;
+    // const [currentIndex, setCurrentIndex] = useState(0);
+
+    // let count = 0;
 
 
-    const startSlider = () => {
-      setInterval(() => {
-        count = (count + 1) % 10
-        setCurrentIndex(count);
-      }, 5000);
-    };
+    // const startSlider = () => {
+    //     setInterval(() => {
+    //         count = (count + 1) % 10
+    //         setCurrentIndex(count);
+    //     }, 5000);
+    // };
 
-    
-  useEffect(() => {
-    startSlider();
-  }, []);
+
+    // useEffect(() => {
+    //     startSlider();
+    // }, []);
     return (
         <div className="h-full max-h-full overflow-y-scroll no-scrollbar snap snap-y snap-mandatory  ">
             <section className='flex flex-col justify-center justify-items-center py-2 w-full h-screen bg-center bg-cover snap-start'>
@@ -32,11 +36,11 @@ const Home = () => {
                     </div>
                     <div id='g' className='relative h-full flex overflow-x-auto'>
                         <div className='flex-shrink-0 rounded-2xl w-fit ml-[5vw]  border-4 border-black backdrop-filter justify-center backdrop-blur-md flex flex-col '>
-                           <div className='bg-slate-100 bg-opacity-30 h-1/2 w-full ml-8 rounded flex flex-col justify-center px-8' >
+                            <div className='bg-slate-100 bg-opacity-30 h-1/2 w-full ml-8 rounded flex flex-col justify-center px-8' >
 
-                            <h1 className='text-black md:text-4xl font-bold  '>Vicariously</h1>
-                            <h2 className='text-slate-700 md:text-3xl font-small md:mt-5 '>Document your greatest travel finds</h2>
-                           </div>
+                                <h1 className='text-black md:text-4xl font-bold  '>Vicariously</h1>
+                                <h2 className='text-slate-700 md:text-3xl font-small md:mt-5 '>Document your greatest travel finds</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -45,27 +49,52 @@ const Home = () => {
             </section>
             <section className='flex flex-col justify-center justify-items-center py-2 w-full h-screen bg-center bg-cover snap-start'>
                 <div className='flex-1' />
-                <div className='relative  mx-8 h-[75vh] md:h-[50vh] '>
+
+
+
+                <div className='relative  mx-8 h-[80vh] md:h-[60vh] '>
                     <div className=' w-full h-full pt-6 pb-8 absolute'>
                         <div className='bg-gradient-to-tr from-blue-300 to-amber-300 rounded-2xl  relative w-full h-full opacity-70'>
                         </div>
                     </div>
-                    <div  className='relative h-full flex overflow-x-auto scrollbar  hover:scroll-auto  space-x-80'>
-                   
+                    <div className='relative h-full flex overflow-x-auto scrollbar  hover:scroll-auto  space-x-20'>
+                    <img src={seoul} alt="map" className=" rounded-2xl w-fit md:w-1/3  border-4 border-black    " />
 
-                        <div className='flex-shrink-0 rounded-2xl w-1/3  border-4 border-black relative overflow-auto	'>
-                        <img src={beach} alt="map" className="absolute w-full h-full object-cover object-bottom   " />
+                        <div className='flex-shrink-0 rounded-2xl w-fit md:w-1/4 ml-[5vw]  border-4 border-black backdrop-filter justify-center backdrop-blur-md flex flex-col '>
+                            <div className='bg-slate-100 bg-opacity-30 h-2/3 w-full ml-4 rounded flex flex-col justify-center px-8' >
+                                <h1 className='text-black md:text-xl font-bold  '>Keep Track of All the <span className="  underline underline-offset-4 decoration-sky-500 ">Cities</span> You Have Visited</h1>
+                            </div>
                         </div>
-                        <div className='flex-shrink-0 rounded-2xl w-1/3  border-4 border-black relative overflow-auto	'>
-                        <img src={beach} alt="map" className="absolute w-full h-full object-cover object-bottom   " />
+
+                        <img src={cities} alt="map" className=" rounded-2xl w-fit md:w-1/3  border-4 border-black    " />
+                        <div className='flex-shrink-0 rounded-2xl w-fit md:w-1/4 ml-[5vw]  border-4 border-black backdrop-filter justify-center backdrop-blur-md flex flex-col '>
+                            <div className='bg-slate-100 bg-opacity-30 h-2/3 w-full ml-4 rounded flex flex-col justify-center px-8' >
+                                <h1 className='text-black md:text-xl font-bold  '>Keep Notes of All the Important Things to Know About that City</h1>
+                            </div>
                         </div>
-                        <div className='flex-shrink-0 rounded-2xl w-1/3  border-4 border-black relative overflow-auto	'>
-                        <img src={beach} alt="map" className="absolute w-full h-full object-cover object-bottom   " />
+                        <img src={cityNotes} alt="map" className=" rounded-2xl w-fit md:w-1/3  border-4 border-black    " />
+                        <div className='flex-shrink-0 rounded-2xl w-fit md:w-1/4 ml-[5vw]  border-4 border-black backdrop-filter justify-center backdrop-blur-md flex flex-col '>
+                            <div className='bg-slate-100 bg-opacity-30 h-2/3 w-full ml-4 rounded flex flex-col justify-center px-8' >
+                                <h1 className='text-black md:text-xl font-bold  '>Keep Track of All the <span className="  underline underline-offset-4 decoration-sky-500 ">Places</span> you Visited in that City</h1>
+                            </div>
+                        </div>
+                        <img src={temple} alt="map" className=" rounded-2xl w-fit md:w-1/3  border-4 border-black    " />
+                        <div className='flex-shrink-0 rounded-2xl w-fit md:w-1/4 ml-[5vw]  border-4 border-black backdrop-filter justify-center backdrop-blur-md flex flex-col '>
+                            <div className='bg-slate-100 bg-opacity-30 h-2/3 w-full ml-4 rounded flex flex-col justify-center px-8' >
+                                <h1 className='text-black md:text-xl font-bold  '>Keep Notes of All the Important Things to Know About that Place:</h1>
+                                <p>Cost | Your Rating | Date Visited | Your Notes | Link | Map </p>
+                            </div>
                         </div>
                     </div>
+
                 </div>
+
+
+
+
+
                 <div className='flex-1' />
-                <ArrowDownIcon class="animate-bounce text-lg  w-7 h-7 mt-auto self-center p-1 font-extrabold bg-white bg-opacity-40 rounded-full shadow-lg border-2 border-slate-200 border-opacity-50  " />
+                {/* <ArrowDownIcon class="animate-bounce text-lg  w-7 h-7 mt-auto self-center p-1 font-extrabold bg-white bg-opacity-40 rounded-full shadow-lg border-2 border-slate-200 border-opacity-50  " /> */}
             </section>
             <section className='w-full h-full bg-blue-200  snap-start'>sec 2</section>
         </div>
